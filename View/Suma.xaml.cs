6c7d7179c832f6656e7cfc039c73264d7f506443
andres <148400031+andres-rivera-05@users.noreply.github.com>
2023-12-08 19:03:32 -0600

    creando la logica para sumar

--- View/Suma.xaml.cs
+++ View/Suma.xaml.cs
@@ -6,4 +6,26 @@ public partial class Suma : ContentPage
 	{
 		InitializeComponent();
 	}
-}
+
+    private void ButtonLimpiar_Clicked(object sender, EventArgs e)
+    {
+        imputUno.Text = string.Empty;
+        imputDos.Text = string.Empty;
+        imputTres.Text = string.Empty;
+
+    }
+
+    private void ButtonSumar_Clicked(object sender, EventArgs e)
+    {
+        if (double.TryParse(imputUno.Text, out double valorUno)&&
+            double.TryParse(imputDos.Text, out double valorDos)&&
+            double.TryParse(imputTres.Text, out double valorTres)){
+
+            double resultado = valorUno + valorDos + valorTres;
+            DisplayAlert("Resultado",$"La Suma es:{resultado}","OK");
+        }
+        else{
+            DisplayAlert("Error", "Completa los campos", "OK");
+        }
+    }
+ }
