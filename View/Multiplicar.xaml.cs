977e69db034965b20dcb510bff486e4c2590e4d8
andres <148400031+andres-rivera-05@users.noreply.github.com>
2023-12-10 17:49:56 -0600

    creando logica para la multiplicacion

--- View/Multiplicar.xaml.cs
+++ View/Multiplicar.xaml.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Opercaciones.View;
 
 public partial class Multiplicar : ContentPage
@@ -5,5 +7,32 @@ public partial class Multiplicar : ContentPage
 	public Multiplicar()
 	{
 		InitializeComponent();
-	}
+
+    }
+
+    private void ButtonMultiplicar_Clicked(object sender, EventArgs e)
+    {
+        if (double.TryParse(imputUno.Text, out double valorUno) &&
+                   double.TryParse(imputDos.Text, out double valorDos) &&
+                   double.TryParse(imputTres.Text, out double valorTres))
+        {
+            double resultado = valorUno * valorDos * valorTres;
+            DisplayAlert("Resultado", $"La Multipiacion es:{resultado}", "Aceptar");
+        }
+        else if(imputUno.Text == "." || imputDos.Text == "." || imputTres.Text == ".")
+        {
+            DisplayAlert("Error", "Completa los campos con numeros", "OK");
+        }
+        else
+        {
+            DisplayAlert("Error", "Completa los campos", "OK");
+        }
+    }
+
+    private void ButtonLimpiar_Clicked(object sender, EventArgs e)
+    {
+        imputUno.Text = string.Empty;
+        imputDos.Text = string.Empty;
+        imputTres.Text = string.Empty;
+    }
 }
