3101b2cb99cc3fe0d7e153a76ada9ce9b0d15fed
andres <148400031+andres-rivera-05@users.noreply.github.com>
2023-12-10 18:19:16 -0600

    creando logica para divicion

--- View/Dividir.xaml.cs
+++ View/Dividir.xaml.cs
@@ -6,4 +6,40 @@ public partial class Dividir : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ButtonDividir_Clicked(object sender, EventArgs e)
+    {
+            if (double.TryParse(imputDividendo.Text, out double valorUno) &&
+                  double.TryParse(imputDivisor.Text, out double valorDos))
+            {
+
+            if (valorDos == 0)
+            {
+                DisplayAlert("Ups", "No se puede dividir entre 0", "Aceptar");
+
+            }
+            else
+            {
+                double resultado = valorUno / valorDos;
+                DisplayAlert("Resultado", $"La Divicion es: {resultado}", "Aceptar");
+            }
+                
+            }
+            else if ((imputDividendo.Text == "." || imputDividendo.Text == "-") ||
+                    (imputDivisor.Text == "." || imputDivisor.Text == "-"))
+            {
+                DisplayAlert("Error", "Completa los campos con numeros", "Aceptar");
+            }
+            else
+            {
+                DisplayAlert("Error", "Completa los campos", "Aceptar");
+            }
+              
+    }
+
+    private void ButtonLimpiar_Clicked(object sender, EventArgs e)
+    {
+        imputDividendo.Text = string.Empty;
+        imputDivisor.Text = string.Empty;
+    }
 }
